d7a2936032b8711335495acc4cafea8798e4d538
Zukka <info@zukkadev.net>
2017-08-24 15:53:00 +0200

    Add save older map position for display it when app is open again

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     // MARK: IBOutlet
     
@@ -17,10 +17,34 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var editButton: UIBarButtonItem!
     
+    // MARK : Properties
+    
+    var userPosition: CLLocationCoordinate2D!
+    var positionManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.positionManager = CLLocationManager()
+        positionManager.delegate = self
+        positionManager.requestWhenInUseAuthorization()
+        positionManager.pausesLocationUpdatesAutomatically = false
+        positionManager.startUpdatingLocation()
+        
         // move infoView down over the screen for initial view
         infoView.transform = CGAffineTransform(translationX: 0, y: 75)
+        
+        if UserDefaults.standard.bool(forKey: "HasLaunchedBefore") {
+            // Retrive previous data
+            let previousLatitude = UserDefaults.standard.double(forKey: "PreviousLatitude")
+            let previousLongitude = UserDefaults.standard.double(forKey: "PreviousLongitude")
+            let previousLatitudeDelta = UserDefaults.standard.double(forKey: "PreviousLatitudeSpan")
+            let previousLongitudeDelta = UserDefaults.standard.double(forKey: "PreviousongitudeSpan")
+            
+            restoreOldMap(prevLatitude: previousLatitude, prevLongitude: previousLongitude, prevLatitudeDelta: previousLatitudeDelta, prevLongitudeDelta: previousLongitudeDelta)
+        } else {
+            UserDefaults.standard.set(true, forKey: "HasLaunchedBefore")
+        }
     }
 
     // MARK: IBAction
@@ -57,6 +81,23 @@ class MapViewController: UIViewController {
         
     }
     
+    // Move map to last position
+    func restoreOldMap(prevLatitude: Double, prevLongitude: Double, prevLatitudeDelta: Double, prevLongitudeDelta: Double) {
+        self.userPosition =  CLLocationCoordinate2DMake(prevLatitude, prevLongitude)
+        let span = MKCoordinateSpanMake(prevLatitudeDelta, prevLongitudeDelta)
+        let region = MKCoordinateRegion(center: userPosition, span: span)
+        self.mapView.setRegion(region, animated: true)
+    }
     
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // save previous position
+        UserDefaults.standard.set(self.mapView.centerCoordinate.latitude, forKey: "PreviousLatitude")
+        UserDefaults.standard.set(self.mapView.centerCoordinate.longitude, forKey: "PreviousLongitude")
+        UserDefaults.standard.set(self.mapView.region.span.latitudeDelta, forKey: "PreviousLatitudeSpan")
+        UserDefaults.standard.set(self.mapView.region.span.longitudeDelta, forKey: "PreviousongitudeSpan")
+    }
 }
 
