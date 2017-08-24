c104f822aef9fbe5f93ca5157e7dbeca9ccb5398
Zukka <info@zukkadev.net>
2017-08-24 08:53:46 +0200

    moved file in new group

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Alessandro Bellotti on 24/08/17.
+//  Copyright © 2017 Alessandro Bellotti. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
